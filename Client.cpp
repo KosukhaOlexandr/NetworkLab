f70a918b2041a80a59c08f45dcb60162f2b19378
KosukhaOlexandr <55558468+KosukhaOlexandr@users.noreply.github.com>
2020-12-02 00:06:21 +0200

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,116 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <winsock2.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define PORT 8800
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    WSADATA WSA_Socket;
+    int iResult = WSAStartup(MAKEWORD(2,2), &WSA_Socket);
+    if (iResult!=0)
+    {
+        printf("\nSocket_Server()> open WSAStartup is invalid!");
+        return -1;
+    }
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+
+    char buffer[1024] = {0};
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // Convert IPv4 and IPv6 addresses from text to binary form
+//    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+//    {
+//        printf("\nInvalid address/ Address not supported \n");
+//        return -1;
+//    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+
+
+    ifstream fin ("put.txt");
+    string arr[1000];
+    int arrlen = 0;
+    while (!fin.eof())
+    {
+        fin >> arr[arrlen++];
+    }
+    while (true)
+    {
+        char hi[1024] = {0};
+        char ex[15] = "exit";
+        string s;
+        cin >> s;
+        if (s == "update")
+        {
+            char checkm[15] = "check_change";
+            strcpy(hi, checkm);
+            send(sock, hi, strlen(hi), 0);
+
+            char nochange[25] = "There are no changes";
+            valread = recv(sock, buffer, 1024, 0);
+            int i = 0;
+            while (i < valread)
+            {
+                if (buffer[i] == '<') {
+                    i++;
+                    int num = 0;
+                    while (buffer[i] != ';') {
+                        num = num * 10 + ((int) buffer[i] - 48);
+                        i++;
+                    }
+                    if (num >= arrlen)
+                        arrlen = num + 1;
+                    i++;
+                    string newstr = "";
+                    while (buffer[i] != '>') {
+                        newstr += buffer[i];
+                        i++;
+                    }
+                    arr[num] = newstr;
+
+                }
+                if (buffer[i] == 'F') {
+                    break;
+                }
+                i++;
+            }
+            printf("%s\n", buffer);
+            memset(buffer, 0, sizeof(*buffer));
+            cout << 1 << '\n';
+
+        }
+        if (s == "exit")
+        {
+
+            send(sock, ex, strlen(ex), 0);
+            cout << "Finish" << '\n';
+            break;
+        }
+
+    }
+    system("pause");
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,135 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <winsock2.h>
+#include <stdlib.h>
+#include <string.h>
+#define PORT 8800
+#include <fstream>
+#include <iostream>
+
+#include <string>
+
+const int MaxCharLength = 1023;// для більшої довжини потрібно додати серіалізацію даних
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ifstream fin ("put.txt");
+    string arr[1000];
+    int arrlen = 0;
+    while (!fin.eof())
+    {
+        fin >> arr[arrlen++];
+    }
+    fin.close();
+    ifstream ch_in("change.txt");
+    string change[1000];
+    int changelen = 0;
+    while (!ch_in.eof())
+    {
+        ch_in >> change[changelen++];
+    }
+
+
+
+    WSADATA WSA_Socket;
+    SOCKET server_fd;
+
+    if ( WSAStartup(MAKEWORD(2,2), &WSA_Socket)!=0)
+    {
+        printf("\nSocket_Server()> open WSAStartup is invalid!");
+        return -1;
+    }
+
+    int valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        perror("socket failed");
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+    //   u_long on = 1;
+    //ioctlsocket(server_fd,FIONBIO,&on);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = htons( PORT );
+
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) == SOCKET_ERROR)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 1) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    SOCKET new_socket;
+
+    while(true) {
+        if ((new_socket = accept(server_fd, nullptr,
+                                 nullptr)) == SOCKET_ERROR)
+        {
+            cout << WSAGetLastError() << '\n';
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        int flag = 1;
+        while (flag) {
+            valread = recv(new_socket, buffer, 1023, 0);
+            if (valread > 0) {
+                char ch[15] = "check_change";
+                if (strcmp(buffer, ch) == 0) {
+                    memset(buffer, 0, sizeof(*buffer));
+                    int k = 0;
+                    int s = 0;
+                    string notificate = ""; // довжина не більше 1024 символів
+                    while (s != max(arrlen, changelen)) {
+                        if (arr[s] != change[s]) {
+                            arr[s] = change[s];
+                            notificate += "<" + to_string(s) + "; " + change[s] + "> ";
+                            printf("Hello message sent\n");
+                            k++;
+                        }
+                        s++;
+                    }
+                    char nochange[25] = "There are no changes";
+                    string sf = "Finish";
+
+                    if (k == 0) {
+                        send(new_socket, nochange, strlen(nochange), 0);
+                        cout << nochange << '\n';
+                    } else {
+                        notificate += sf;
+                        char *pchar = new char[MaxCharLength + 1];
+                        strcpy(pchar, notificate.c_str());
+                        send(new_socket, pchar, strlen(pchar), 0);
+                        cout << notificate << '\n';
+                    }
+                }
+                char ex[15] = "exitk_change";// костиль
+                if (strcmp(buffer, ex) == 0) {
+                    memset(buffer, 0, sizeof(*buffer));
+                    printf("Exit\n");
+                    break;
+                }
+            } else
+                flag = 0;
+        }
+    }
+    closesocket(server_fd);
+    closesocket(new_socket);
+    fin.close();
+    ch_in.close();
+    WSACleanup();
+    system("pause");
+
+    return 0;
+}
